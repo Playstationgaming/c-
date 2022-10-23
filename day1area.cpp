e20aaca2d78914605ab648b76f95c346e4c13baa
raj15chat <116450136+raj15chat@users.noreply.github.com>
2022-10-23 15:33:13 +0530

    Add files via upload

---
+++ day1area.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main () {
+    int len,bre;
+    cout<<"lenght";
+    cin>>len;
+    cout<<"breath";
+    cin>>bre;
+    int area;
+    area=len*bre;
+    cout<<"area="<<area;
+    return 0;
+}
